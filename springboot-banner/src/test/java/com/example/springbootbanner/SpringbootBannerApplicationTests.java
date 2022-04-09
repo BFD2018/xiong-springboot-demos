0fc0b3bbe76766430c3f37274b85337b1d049fff
XIONG <1351655382@qq.com>
2022-04-09 22:14:20 +0800

    【用户：xjt】【修改模块：springboot-banner】 【修改说明：修改启动banner图】

---
+++ springboot-banner/src/test/java/com/example/springbootbanner/SpringbootBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
