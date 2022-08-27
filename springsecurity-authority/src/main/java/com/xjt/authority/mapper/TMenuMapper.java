fbcbc3f01940c70a868d67b2e2011be4c1ef39fd
XIONG <1351655382@qq.com>
2022-08-27 11:20:53 +0800

    【author：xjt】【commit：spring security】 【description：基于数据库的认证+基于URL的授权】

--- springsecurity-authority/src/main/java/com/xjt/authority/mapper/TMenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xjt.authority.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TMenuMapper {
-}
--- springsecurity-authority/src/main/java/com/xjt/authority/service/TMenuService.java
+++
@@ -1,9 +0,0 @@
-package com.xjt.authority.service;
-
-import com.xjt.authority.entity.TMenu;
-
-import java.util.List;
-
-public interface TMenuService {
-    List<TMenu> getAllMenu();
-}
